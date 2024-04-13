6340d961fda77aa66fec4db1b238c90408c5d826
daichi5 <sawa22daichi@gmail.com>
2024-04-13 14:28:14 +0900

    add sample plugin

---
+++ nuxt/plugins/myPlugin.ts
@@ -0,0 +1,20 @@
+export default defineNuxtPlugin({
+  name: 'my-plugin',
+  async setup (nuxtApp) {
+    // this is the equivalent of a normal functional plugin
+    console.log('setup called in my-plugin')
+  },
+  hooks: {
+    // You can directly register Nuxt app runtime hooks here
+    'app:created'() {
+      const nuxtApp = useNuxtApp()
+      // do something in the hook
+      console.log('app:created hook called in my-plugin')
+    },
+
+    "vue:setup": () => {
+      // do something in the hook
+      console.log('vue:setup hook called in my-plugin')
+    }
+  },
+})
