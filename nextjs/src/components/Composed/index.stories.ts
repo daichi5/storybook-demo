dad509125a94415c4072417f0da6a6311a26fc33
daichi5 <sawa22daichi@gmail.com>
2024-04-17 22:17:16 +0900

    add play function

---
+++ nextjs/src/components/Composed/index.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Composed } from './index';
+import { userEvent, within } from '@storybook/test';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Composed',
+  component: Composed,
+  parameters: {},
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  args: { },
+} satisfies Meta<typeof Composed>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const inner = canvas.getByTestId('inner');
+    const innerCanvas = within(inner);
+
+
+    const button1 = canvas.getByText('Button1');
+    const button2 = canvas.getByText('Button2');
+    const TextField = innerCanvas.getByLabelText('Text Field');
+
+    await userEvent.click(button1);
+    await userEvent.type(TextField, 'Hello, World!', { delay: 100 });
+
+    await userEvent.click(canvas.getByText('Button2'));
+  }
+};
---
+++ nextjs/src/components/Composed/index.tsx
@@ -0,0 +1,22 @@
+import { Stack } from '@/components/Stack'
+import { Cluster } from '@/components/Cluster'
+import { Text } from '@/components/Text'
+import { TextField } from '@/components/TextField'
+import { Button } from '@/components/Button'
+
+export const Composed: React.FC = () => {
+  return <Stack>
+    <Cluster>
+      <Text>Composed</Text>
+      <Text>description1</Text>
+    </Cluster>
+    <Stack>
+      <div data-testid="inner">
+        <TextField />
+        <Button>Button1</Button>
+      </div>
+    </Stack>
+
+    <Button>Button2</Button>
+  </Stack>
+}
